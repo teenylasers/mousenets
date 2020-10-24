0052127ce9e0e2225ef928c95657bf21eec902ee
teenylasers <teenylasers@gmail.com>
2020-10-23 17:53:17 -0700

    test tf implementations

---
+++ unittest_utils.py
@@ -0,0 +1,122 @@
+import random, unittest, statistics
+import tensorflow as tf
+import numpy as np
+from constants import *
+from utils import *
+
+
+class TestNormalizeData(unittest.TestCase):
+    """ """
+    def test_normalize_data(self):
+        """Test normalize_data function for both numpy array and tf.Variable."""
+        num_tests = 3
+        x_dims=[2,3,4,8,16]
+        y_dims=[2,3,4,8,16]
+        z_dims=[2,3,4,8,16]
+
+        # Test 2D matrices
+        for ii in range(num_tests):
+            x_np = np.random.rand(random.choice(x_dims), random.choice(y_dims))
+            x_tf = tf.Variable(x_np)
+
+            x_np = normalize_data(x_np)
+            self.assertAlmostEqual(tf.math.reduce_mean(x_np).numpy(), 0, 6,
+                                   'mean = {}'.format(tf.math.reduce_mean(x_np)))
+            self.assertAlmostEqual(tf.math.reduce_std(x_np).numpy(), 1, 6,
+                                   'stdev = {}'.format(tf.math.reduce_std(x_np)))
+
+            x_tf = normalize_data(x_tf)
+            self.assertAlmostEqual(tf.math.reduce_mean(x_tf).numpy(), 0, 6,
+                                   'mean = {}'.format(tf.math.reduce_mean(x_tf)))
+            self.assertAlmostEqual(tf.math.reduce_std(x_tf).numpy(), 1, 6,
+                                   'stdev = {}'.format(tf.math.reduce_std(x_tf)))
+
+        # Test 3D matrices
+        for ii in range(num_tests):
+            x_np = np.random.rand(random.choice(x_dims), random.choice(y_dims),
+                                  random.choice(z_dims))
+            x_tf = tf.Variable(x_np)
+            x_np = normalize_data(x_np)
+
+            self.assertAlmostEqual(tf.math.reduce_mean(x_np).numpy(), 0, 6,
+                                   'mean = {}'.format(tf.math.reduce_mean(x_np)))
+            self.assertAlmostEqual(tf.math.reduce_std(x_np).numpy(), 1, 6,
+                                   'stdev = {}'.format(tf.math.reduce_std(x_np)))
+
+            x_tf = normalize_data(x_tf)
+            self.assertAlmostEqual(tf.math.reduce_mean(x_tf).numpy(), 0, 6,
+                                   'mean = {}'.format(tf.math.reduce_mean(x_tf)))
+            self.assertAlmostEqual(tf.math.reduce_std(x_tf).numpy(), 1, 6,
+                                   'stdev = {}'.format(tf.math.reduce_std(x_tf)))
+
+        # Test 1D vectors
+        for ii in range(num_tests):
+            x_np = np.random.rand(random.choice(x_dims))
+            x_tf = tf.Variable(x_np)
+
+            x_np = normalize_data(x_np)
+            self.assertAlmostEqual(tf.math.reduce_mean(x_np).numpy(), 0, 6,
+                                   'mean = {}'.format(tf.math.reduce_mean(x_np)))
+            self.assertAlmostEqual(tf.math.reduce_std(x_np).numpy(), 1, 6,
+                                   'stdev = {}'.format(tf.math.reduce_std(x_np)))
+
+            x_tf = normalize_data(x_tf)
+            self.assertAlmostEqual(tf.math.reduce_mean(x_tf).numpy(), 0, 6,
+                                   'mean = {}'.format(tf.math.reduce_mean(x_tf)))
+            self.assertAlmostEqual(tf.math.reduce_std(x_tf).numpy(), 1, 6,
+                                   'stdev = {}'.format(tf.math.reduce_std(x_tf)))
+
+
+
+class TestImageToPoints(unittest.TestCase):
+    """Test image_to_points function, using mnist images stored as a numpy array
+    or a tf.Variable."""
+
+    def _load_mnist_samples(self):
+        """Load local MNIST samples x_train_*.npy."""
+        num_samples = 30
+        samples = [np.load('mnist_samples/x_train_%d.npy' % it) for it in
+                   range(num_samples)]
+        return samples
+
+
+    def _test_image_to_points_np(self, images):
+        """Test image_to_points with numpy.array input."""
+        num_tests = 2
+        thresholds = [60, 250]
+        num_points = [128, 512, 1024]
+
+        # First test default threshold and num_points
+        for ii in range(num_tests):
+            image = random.choice(images)
+            image_to_points(image=image, viz=True)
+
+        # Test various thresholds and num_points
+        for ii in range(num_tests):
+            for th in thresholds:
+                for points in num_points:
+                    image = random.choice(images)
+                    image_to_points(image=image, threshold=th, num_points=points,
+                                    viz=True)
+
+
+    def _test_image_to_points_tf(self, images):
+        """Test image_to_points with tf.Variable input."""
+        pass
+
+
+    def test_image_to_points(self):
+
+        samples = self._load_mnist_samples()
+        self._test_image_to_points_np(samples)
+
+
+
+# class TestConvolution(unittest.TestCase):
+#     """
+#     Test and time the convolution implementations.
+#     """
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utils.py
+++ utils.py
@@ -1,59 +1,67 @@
-import numpy as np
+import random
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def convolution():
+    pass
+
 
 
 def normalize_data(x):
-    """Normalize x, x can be a scalar, a vector, or a matrix."""
+    """Normalize x, x can be a vector or a matrix."""
+    data_size = tf.cast(tf.size(x), tf.float64)
+    assert(data_size > 1)
     # Normalize the mean
-    norm_mean = np.mean(x)
+    norm_mean = tf.math.reduce_mean(x)
     x = x - norm_mean
     # Normalize the variance
-    norm_factor = np.sqrt(np.sum(np.square(x)) / x.size)
+    norm_factor = tf.math.reduce_std(x)
     assert norm_factor != 0
     return x * 1.0 / norm_factor
 
 
-def image_to_points(array, threshold=128, num_points=256, viz=False):
+def image_to_points(image, threshold=128, num_points=256, viz=False):
     """Construct 2D point cloud representation for a grayscale image, keeping
     all pixels with a value greater than threshold. Delete or pad points so the
-    result contains num_points. array is the input image as a nunmpy array. If
+    result contains num_points. image is the input image as a numpy array. If
     viz, show the result as a scatter plot and the original data as an image."""
 
-    dims = x_train[i].shape
+    dims = image.shape
+    if num_points > image.size:
+        print('Warning: num_points [{}] > num pixels in image [{}], use {}.'.format(
+            num_points, image.size, image.size))
+        num_points = image.size
 
     # Get points by the threshold
-    mask = x_train[i] >= threshold
-    s = np.empty((0,3), int)
-    for j in range(dims[0]):
-        for k in range(dims[1]):
-            if mask[j][k] != 0:
-                s = np.vstack((s, [j, k, array[j][k]]))
+    mask = tf.constant(image >= threshold)
+    # Start a first row of zeros to concatenate points to
+    s = [[j, k, image[j][k]] for j in range(dims[0]) for k in range(dims[1])
+         if mask[j][k]]
 
     # Delete or pad points to get num_points
-    size_diff = s.shape[0] - num_points
+    size_diff = len(s) - num_points
     if size_diff > 0:
-        for it in range(size_diff):
-            reject_row = np.random.randint(0,s.shape[0])
-            s = np.delete(s, (reject_row), axis=0)
+        s = s[:-size_diff]
     elif size_diff < 0:
         for it in range(-size_diff):
-            add_row = np.random.randint(0,s.shape[0])
-            s = np.vstack((s, s[add_row]))
+            s.append(random.choice(s))
     else:
         pass
+
+    # Shuffle up the points, so they are not ordered by x or y.
+    s = tf.stack(s)
+    s = tf.random.shuffle(s)
     assert(s.shape[0] == num_points), 's.shape = {}, num_points = {}'.format(
         s.shape, num_points)
 
     # Visualize the 2D point cloud and the original image
     if viz:
-        plt.imshow(x_train[i])
+        plt.imshow(image)
         plt.show()
         plt.scatter(s[:,1], -s[:,0], s=s[:,2]/10)
         plt.xlim(0, dims[1])
         plt.ylim(-dims[0], 0)
         plt.show()
-        print(s.shape)
-
-    # Shuffle up the points, so they are not ordered by x or y.
-    np.random.shuffle(s)
 
     return s
